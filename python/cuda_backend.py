97c9661368d9ae70b81bbb156100773990969925
ishan-renjen <ishanrenjen@gmail.com>
2026-03-23 18:50:11 -0400

    this should work as a basic implementation with CUDA files compiled as a pytorch extension, no cmake needed

---
+++ python/cuda_backend.py
@@ -0,0 +1,7 @@
+from torch.utils.cpp_extension import load #https://docs.pytorch.org/docs/stable/cpp_extension.html
+import torch
+
+flash_attn = load(name="flash_attn_ext", sources=["./src/FlashAttention.cu"], verbose=True)
+
+forward = flash_attn.forward_pass(0)
+backward = flash_attn.backward_pass(0)
--- src/FlashAttention.cu
+++ src/FlashAttention.cu
@@ -1,11 +1,27 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <torch/types.h>
+#include <torch/extension.h>
+
+#include "FlashAttention.h"
 
 __global__ void forwardKernel(){
-    
+
 }
 
 __global__ void backwardKernel(){
 
+}
+
+torch::Tensor forward_pass(torch::Tensor input){
+
+}
+
+torch::Tensor backward_pass(torch::Tensor input){
+    return torch::zeros_like(input);
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("forward_custom", &forward_pass, "Custom forward pass (CUDA)");
+    m.def("backward_custom", &backward_pass, "Custom backward pass (CUDA)");
 }
