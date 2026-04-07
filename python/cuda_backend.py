36d0feb6273436a4fac5310729a9a37de886f1b3
ishan-renjen <ishanrenjen@gmail.com>
2026-04-07 14:12:46 -0400

    fixed minor issues

--- python/cuda_backend.py
+++ python/cuda_backend.py
@@ -3,5 +3,5 @@ import torch
 
 flash_attn = load(name="flash_attn_ext", sources=["./src/FlashAttention.cu"], verbose=True)
 
-forward = flash_attn.forward_pass(0)
-backward = flash_attn.backward_pass(0)
+forward = flash_attn.forward_custom(torch.tensor(0))
+backward = flash_attn.backward_custom(torch.tensor(0))
