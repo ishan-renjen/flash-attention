b33f7d3c248388c0570149bfe39c92f9ca04fdb4
ishan-renjen <ishanrenjen@gmail.com>
2026-04-12 02:20:00 -0400

    added some code for the model

---
+++ python/model/model.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+
+#https://medium.com/@alejandro.itoaramendia/decoding-the-u-net-a-complete-guide-810b1c6d56d8
+#https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_parts.py -- this looks slightly incorrect in places
+
+"""U-Net double convolutional block - 3x3 conv, normalize, ReLU"""
+class UNetConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetConvBlock, self).__init__()
+        self.unet_conv_block = nn.Sequential(
+                               nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                               nn.BatchNorm2d(out_channels),
+                               nn.ReLU(),
+                               nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+                               nn.BatchNorm2d(out_channels),
+                               nn.ReLU())
+
+    def forward(self, x):
+        return self.unet_conv_block(x)
+
+"""single decoder block - upscales then double conv block"""
+class UNetDecodeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetDecodeBlock, self).__init__()
+        self.upscale = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.convblock1 = UNetConvBlock(in_channels, out_channels) #not in_channels//2 b/c it gets concatenated with equal dimensions from skip connection
+
+    def forward(self, x, y):
+        x = self.upscale(x)
+        x = torch.cat([x, y], dim=1) #this may cause issues, i was not sure how to do the concatenation for the skip connection
+        return self.convblock1(x)
+
+"""single encoder block - 2x pooling then double conv block"""
+class UNetEncodeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetEncodeBlock, self).__init__()
+        self.unet_encode_block = nn.Sequential(
+                                 nn.MaxPool2d(kernel_size=2, stride=2),
+                                 UNetConvBlock(in_channels=in_channels, out_channels=out_channels))
+
+    def forward(self, x):
+        return self.unet_encode_block(x)
+
+"""currently unused - replaced with multi head self attention in u-net bottleneck."""
+# class ResidualBlock(nn.Module):
+#     def __init__(self, in_channels):
+#         super(ResidualBlock, self).__init__()
+#         self.residual_block = nn.Sequential(
+#                                nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+#                                nn.BatchNorm2d(in_channels),
+#                                nn.ReLU(),
+#                                nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+#                                nn.BatchNorm2d(in_channels))
+
+#         self.relu = nn.ReLU()
+
+#     def forward(self, x):
+#         x0 = self.residual_block(x)
+#         return self.relu(x + x0)
+
+"""put together the encode and decode blocks. This is 4 encode layers with 4 decode layers and 1 intermediate encode layer"""
+class Generator(nn.Module):
+    def __init__(self, channels, classes):
+        super(Generator, self).__init__()
+        self.channels = channels
+        self.classes = classes
+
+        #start model
+        self.encode1    = UNetConvBlock(channels, 64)
+        self.encode2    = UNetEncodeBlock(64, 128)
+        self.encode3    = UNetEncodeBlock(128, 256)
+        self.encode4    = UNetEncodeBlock(256, 512)
+        self.encode5    = UNetEncodeBlock(512, 1024)
+        self.encode6    = UNetEncodeBlock(1024, 2048)
+        self.attention1 = nn.MultiheadAttention(2048, 6)
+        self.attention2 = nn.MultiheadAttention(2048, 6)
+        self.attention3 = nn.MultiheadAttention(2048, 6)
+        self.decode1    = UNetDecodeBlock(2048, 1024)
+        self.decode2    = UNetDecodeBlock(1024, 512)
+        self.decode3    = UNetDecodeBlock(512, 256)
+        self.decode4    = UNetDecodeBlock(256, 128)
+        self.decode5    = UNetDecodeBlock(128, 64)
+        self.output     = nn.Conv2d(64, classes, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x0 = self.encode1(x)
+        x1 = self.encode2(x0)
+        x2 = self.encode3(x1)
+        x3 = self.encode4(x2)
+        x4 = self.encode5(x3)
+        x5 = self.encode6(x4)
+        x5 = self.attention1(x5)
+        x5 = self.attention2(x5)
+        x5 = self.attention3(x5)
+        x  = self.decode1(x5,x4)
+        x  = self.decode2(x,x3)
+        x  = self.decode3(x,x2)
+        x  = self.decode4(x,x1)
+        x  = self.decode5(x,x0)
+        return self.output(x)
+    
+#Discriminator model
+#https://sahiltinky94.medium.com/understanding-patchgan-9f3c8380c207
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, num_filters=64):
+        super(Discriminator, self).__init__()
+
+        self.layer1 = nn.Sequential((nn.Conv2d(in_channels=in_channels, out_channels=num_filters, kernel_size=4, stride=2, padding=1)),
+                                    nn.LeakyReLU(0.1))
+
+        self.layer2 = nn.Sequential((nn.Conv2d(in_channels=num_filters, out_channels=num_filters*2, kernel_size=4, stride=2, padding=1)),
+                                    nn.BatchNorm2d(num_filters*2),
+                                    nn.LeakyReLU(0.1))
+
+        self.layer3 = nn.Sequential((nn.Conv2d(in_channels=num_filters*2, out_channels=num_filters*4, kernel_size=4, stride=2, padding=1)),
+                                    nn.BatchNorm2d(num_filters*4),
+                                    nn.LeakyReLU(0.1))
+
+        self.layer4 = nn.Sequential((nn.Conv2d(in_channels=num_filters*4, out_channels=num_filters*8, kernel_size=4, stride=2, padding=1)),
+                                    nn.BatchNorm2d(num_filters*8),
+                                    nn.LeakyReLU(0.1))
+
+        self.layer5 = (nn.Conv2d(num_filters*8, 1, kernel_size=4, stride=1, padding=1))
+
+
+    def forward(self, x):
+        output = self.layer1(x)
+        output = self.layer2(output)
+        output = self.layer3(output)
+        output = self.layer4(output)
+        output = self.layer5(output)
+        return output
